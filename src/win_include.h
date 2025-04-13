6a64da534290ab9dcd62ac601d6fef7fdeb57509
hai <ngo.chu.le.vuong@gmail.com>
2025-04-13 23:22:13 +0700

    fix on windows: resolve 'byte' ambiguity with <windows.h> include

--- src/win_include.h
+++ src/win_include.h
@@ -1,6 +1,19 @@
 #pragma once
 
 #ifdef _WIN32
-#define NOMINMAX
-#include <windows.h>
+// prevent include many unimportant headers
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 #endif
+
+// prevent conflict with std::min / std::max
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+// prevent conflict 'byte' between Windows API to std::byte
+#define byte windows_byte_workaround
+#include <windows.h>
+#undef byte
+
+#endif // _WIN32
